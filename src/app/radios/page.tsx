88a25dd3b46914ea851d95d660beba69057aa253
Kyle Esterhuizen <kyle@knetcode.com>
2023-10-11 20:00:30 +0200

    remove readme, fix nav

---
+++ src/app/radios/page.tsx
@@ -0,0 +1,7 @@
+export default function Radios() {
+  return (
+    <div>
+      <h1>Radios</h1>
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,31 @@
 import Link from 'next/link';
 
 export default function Navbar() {
+  const itemsInCart = 0;
+
   return (
     <nav className="flex items-center justify-between px-8 py-4 shadow-lg">
       <Link href="/" className="text-xl font-bold text-teal-500">
         NSRadio
       </Link>
       <div className="flex items-center justify-center gap-8">
-        <div>Products</div>
-        <div>Cart</div>
+        <Link
+          href="/products"
+          className="group transition-all hover:text-teal-500 "
+        >
+          Products
+          <div className="h-[2px] w-0 bg-teal-500 transition-all group-hover:w-full" />
+        </Link>
+        <Link
+          href="/cart"
+          className="group transition-all hover:text-teal-500 "
+        >
+          Cart{' '}
+          <span className="text-lg font-semibold text-teal-500">
+            {itemsInCart > 0 && itemsInCart}
+          </span>
+          <div className="h-[2px] w-0 bg-teal-500 transition-all group-hover:w-full" />
+        </Link>
       </div>
     </nav>
   );
